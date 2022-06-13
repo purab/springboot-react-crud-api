d032d63ef0fe66ba3ff98fdfb9c581307e4d7843
purab <purabdk@yahoo.co.in>
2022-06-13 22:04:21 +0530

    add new files....

---
+++ src/main/webapp/react_ui/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header" style={{background:"red"}}>
+        header
+        <p>this is header component</p>
+        </div>
+    )
+}
+
+export default Header;
